a6f7f9e120dda1384dcec09bfdb9638c2836e093
A1abhiral <mr.abhiral.p@gmail.com>
2024-09-30 00:39:42 +0545

    database connection

--- ablog/settings.py
+++ ablog/settings.py
@@ -92,8 +92,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
+POSTGRES_LOCALLY = False
+if ENVIRONMENT == 'production'or POSTGRES_LOCALLY == True:
+    DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 
 # Password validation
