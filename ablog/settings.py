f04743d5ebc51be5d0c4878c0b088dca084ec6be
A1abhiral <mr.abhiral.p@gmail.com>
2024-09-30 00:07:47 +0545

    change

--- ablog/settings.py
+++ ablog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 from environ import Env
 env = Env()
@@ -91,6 +92,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 
 # Password validation
--- theblog/forms.py
+++ theblog/forms.py
@@ -1,42 +1,41 @@
 from django import forms
-from theblog.models import Post,Category 
-
-choices = Category.objects.all().values_list('name', 'name')
-
-choice_list = []
-
-for i in choices:
-    choice_list.append(i)#
+from theblog.models import Post, Category
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('header_image','title','author','category','body','snippet')
+        fields = ('header_image', 'title', 'author', 'category', 'body', 'snippet')
 
         widgets = {
-            'title' : forms.TextInput(attrs={'class':'form-control',}),
-            'author' : forms.TextInput(attrs={'class':'form-control','value':'','id':'elder','type':'hidden'}),
-            'header_image' : forms.FileInput(attrs={'class': 'form-control'}),
-            'category' : forms.Select(choices=choice_list,attrs={'class':'form-control',}),
-            'body' : forms.Textarea(attrs={'class':'form-control',}),
-            'snippet' : forms.Textarea(attrs={'class':'form-control',}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
+            'header_image': forms.FileInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        # Dynamically fetch categories and set them as choices
+        choices = Category.objects.all().values_list('name', 'name')
+        self.fields['category'].widget = forms.Select(choices=choices, attrs={'class': 'form-control'})
 
-        } 
 
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title','body','header_image','snippet','category')
+        fields = ('title', 'body', 'header_image', 'snippet', 'category')
 
         widgets = {
-            'title' : forms.TextInput(attrs={'class':'form-control',}),
-            'body' : forms.Textarea(attrs={'class':'form-control',}),
-            'category' : forms.Select(choices=choice_list,attrs={'class':'form-control',}),
-            'snippet' : forms.Textarea(attrs={'class':'form-control',}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
             'header_image': forms.FileInput(attrs={'class': 'form-control'}),
-
-
         }
 
-
+    def __init__(self, *args, **kwargs):
+        super(UpdateForm, self).__init__(*args, **kwargs)
+        # Dynamically fetch categories and set them as choices
+        choices = Category.objects.all().values_list('name', 'name')
+        self.fields['category'].widget = forms.Select(choices=choices, attrs={'class': 'form-control'})
